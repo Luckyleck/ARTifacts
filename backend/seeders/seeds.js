b8245ca268da5e0b1d422502f64ada62ed488ec5
StevenNotovitz <snotovitz@gmail.com>
2023-04-20 17:41:22 -0400

    .

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -3,16 +3,17 @@ const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
-const pikachu = require('../../frontend/src/components/NavBar/assets/pikachu.png');
-const starry = require('../../frontend/src/components/MainPage/assets/starry_night.webp');
+const fs = require('fs');
+const path = require('path');
+const pikachu = fs.readFileSync(path.join(__dirname, '../../frontend/src/components/NavBar/assets/pikachu.png'));
+const starry = fs.readFileSync(path.join(__dirname, '../../frontend/src/components/MainPage/assets/starry_night.webp'));
+
 
 const NUM_SEED_USERS = 1000;
 
 // Create users
 const users = [];
 
-const randomSeed1 = faker.random.alphaNumeric(5);
-const randomSeed2 = faker.random.alphaNumeric(5);
 users.push(
   new User({
     username: 'demo-user',
