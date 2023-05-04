e573cd6e747ad1f04c459bd670022eea1078af9e
Kevin Chan <chankevin13@gmail.com>
2023-05-04 18:02:44 -0400

    style

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -3,12 +3,9 @@ import './SearchBar.css'
 
 function SearchBar() {
     return (
-        // <div className="search-bar">
-        //     <input type="text" placeholder="Search for users here..."/>
-        // </div>
-    <div className='searchWithIcon'>
-        <i className="fa-solid fa-magnifying-glass" id='searchIcon'></i>
-        <input type="text" placeholder='Search' id='searchBar'/>
+    <div className='searchbar-container'>
+        <i className="fa-solid fa-magnifying-glass search-icon"></i>
+        <input type="text" placeholder='Search' className='search-bar'/>
     </div>
     )
 }
