095ad470815cf983644f07772d5960e18e3479bc
Kevin Chan <chankevin13@gmail.com>
2024-11-06 22:39:42 -0500

    implemented preload screen

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,65 +1,60 @@
-import './MainPage.css';
-import logo from './assets/ART_white.png';
-import cafe from './assets/Cafe_Terrace_at_Night.png';
-import girl from './assets/Girl_with_a_Pearl_Earring.png';
-import guernica from './assets/Guernica.png';
-import mona from './assets/mona.png';
-import starry from './assets/starry_night.png';
-import adam from './assets/The_Creation_of_Adam.png';
-import supper from './assets/The_Last_Supper.png';
-import memory from './assets/The_Persistence_of_Memory.png';
-import scream from './assets/The_scream.png';
-import mg from './assets/mg.png';
-import self from './assets/Self-Portrait.png';
-import monkey from './assets/Tropical_Forest_with_Monkeys.png';
-import flower from './assets/Flowers_in_a_Rococo_Vase.png';
-import poodle from './assets/White_Poodle_in_a_Punt.png';
-import wave from './assets/Under_the_Wave_off_Kanagawa.png';
-import gothic from './assets/American_Gothic.png';
-import sunflowers from './assets/Sunflowers.png';
-import bedroom from './assets/bedroom.png';
-import nighthawks from './assets/Nighthawks.png';
-import al from './assets/al.PNG';
-import sn from './assets/sn.PNG';
-import kc from './assets/kc.PNG';
-import { useHistory } from 'react-router-dom';
+import "./MainPage.css";
+import logo from "./assets/ART_white.png";
+import images from "./index.js";
+import loadingLogo from "../NavBar/assets/ART.png";
+import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const MainPage = () => {
   const history = useHistory();
+  const [isLoading, setIsLoading] = useState(true);
 
-    return (
-      <>
-        <div className='image-container'>
-          <img src={cafe} alt='cafe' className='animated-image' />
-          <img src={girl} alt='girl' className='animated-image' />
-          <img src={guernica} alt='guernica' className='animated-image' />
-          <img src={mona} alt='mona' className='animated-image' />
-          <img src={starry} alt='starry' className='animated-image' />
-          <img src={adam} alt='adam' className='animated-image' />
-          <img src={supper} alt='supper' className='animated-image' />
-          <img src={memory} alt='memory' className='animated-image' />
-          <img src={scream} alt='scream' className='animated-image' />
-          <img src={self} alt='self' className='animated-image' />
-          <img src={monkey} alt='monkey' className='animated-image' />
-          <img src={flower} alt='flower' className='animated-image' />
-          <img src={sunflowers} alt='sunflowers' className='animated-image' />
-          <img src={poodle} alt='poodle' className='animated-image' />
-          <img src={gothic} alt='gothic' className='animated-image' />
-          <img src={wave} alt='wave' className='animated-image' />
-          <img src={bedroom} alt='bedroom' className='animated-image' />
-          <img src={nighthawks} alt='nighthawks' className='animated-image' />
-          {/* <img src={mg} alt='mg' className='animated-image' />
-          <img src={al} alt='al' className='animated-image' />
-          <img src={sn} alt='sn' className='animated-image' />
-          <img src={kc} alt='kc' className='animated-image' /> */}
+  useEffect(() => {
+    const loadImages = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+      });
+    });
+
+    Promise.all(loadImages).then(() => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    });
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <>
+          <div className="reveal" />
+          <div className="preloader">
+            <img src={loadingLogo} alt="logo" />
+          </div>
+        </>
+      ) : (
+        <div className="main-content">
+          <div className="image-container">
+            {images.map((image, index) => (
+              <img
+                key={`artwork ${index}`}
+                src={image}
+                alt={`artwork ${index}`}
+                className={`animated-image ${!isLoading ? "loaded" : ""}`}
+              />
+            ))}
+          </div>
+
+          <div onClick={() => history.push("/explore")} className="explore">
+            <img src={logo} alt="logo" />
+            <p>CLICK HERE TO ENTER</p>
+          </div>
         </div>
-
-      <div onClick={() => history.push('/explore')} className='explore'>
-        <img src={logo} alt='logo' />
-        <p>CLICK HERE TO ENTER</p>
-      </div>
+      )}
     </>
   );
 };
-  
+
 export default MainPage;
